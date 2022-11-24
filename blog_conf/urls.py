0084faf574a94969dd0773af6eefa21dee65ea19
JoseMGuerra <Josem_guerra@hotmail.com>
2022-11-24 20:27:37 +0000

    Add static urlpattern

--- blog_conf/urls.py
+++ blog_conf/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from home.views import home_view
 
 urlpatterns = [
@@ -24,3 +26,9 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', home_view, name='homepage'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
